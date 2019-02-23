b33f6a5247f4ce9a362737d87af36ead73e5269c
cbarnhouse <clbarnhouse01@student.ysu.edu>
2019-02-23 15:48:09 -0500

    made all the models

--- DIT/models.py
+++ DIT/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here
+
+# extend users
+class extended_users(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_pc = models.BooleanField()
+    is_dm = models.BooleanField()
+
+class characters(models.Model):
+    users_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    character_name = models.CharField(max_length=50)
+    character_health = models.IntegerField()
+    character_level = models.IntegerField()
+    character_armor_class = models.IntegerField()
+    character_initiative = models.IntegerField(null=True)
+
+class dm_session(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class creatures(models.Model):
+    dm_session_id = models.ForeignKey(dm_session, on_delete=models.CASCADE)
+    creature_name = models.CharField(max_length=50)
+    creature_health = models.IntegerField()
+    creature_level = models.IntegerField()
+    creature_armor_class = models.IntegerField()
+    creature_initiative = models.IntegerField(null=True)
+
+
+
+
